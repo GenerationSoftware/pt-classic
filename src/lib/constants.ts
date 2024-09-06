1939893e19a2e2613cf4c596e3b003cc0eded57a
Ncookiez <ncukiert@gmail.com>
2024-09-06 15:15:30 -0400

    added pooly art + minor fixes

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,6 @@ import { getDefaultConfig } from '@rainbow-me/rainbowkit'
 import { QueryClient } from '@tanstack/react-query'
 import { getPublicClient } from 'wagmi/actions'
 import { appName, chain } from './config'
-import { http } from 'wagmi'
 
 export const wagmiConfig = getDefaultConfig({
   appName,
