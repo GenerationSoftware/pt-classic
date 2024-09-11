3e66e5e8537649e7d7decc1b438b2e8516370339
Ncookiez <ncukiert@gmail.com>
2024-09-11 19:25:12 -0400

    added back button to sub-account pages

--- src/routes/account/bonus/+page.svelte
+++ src/routes/account/bonus/+page.svelte
@@ -1,6 +1,11 @@
+<script>
+  import { goto } from '$app/navigation'
+  import BackButton from '$lib/components/BackButton.svelte'
+</script>
+
 <!-- TODO: how bonus rewards available to be claimed -->
 <!-- TODO: button to claim and zap into prize vault -->
 <!-- TODO: button to just claim -->
-<!-- TODO: button to go back to account page -->
 
 <span>WE BUIDLING</span>
+<BackButton onClick={() => goto('/account')} />
--- src/routes/account/prizes/+page.svelte
+++ src/routes/account/prizes/+page.svelte
@@ -1,5 +1,10 @@
+<script>
+  import { goto } from '$app/navigation'
+  import BackButton from '$lib/components/BackButton.svelte'
+</script>
+
 <!-- TODO: plinko game to check wins -->
 <!-- TODO: show prizes won -->
-<!-- TODO: button to go back to account page -->
 
 <span>WE BUIDLING</span>
+<BackButton onClick={() => goto('/account')} />
