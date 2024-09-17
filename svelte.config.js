4d9c56e234b29b5356f9caa6583e2e4a442be9b3
Charles Bergeron <chuck.bergeron@gmail.com>
2024-09-17 13:28:47 -0700

    attempting to use sveltejs-adapter-ipfs to fix relative vs absolute URL bugs

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,16 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import reactPreprocess from 'svelte-preprocess-react/preprocessReact'
-import adapter from '@sveltejs/adapter-static'
+import adapter from 'sveltejs-adapter-ipfs'
+// import adapter from '@sveltejs/adapter-static'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: [vitePreprocess(), reactPreprocess()],
-  kit: { adapter: adapter() }
+  kit: {
+    adapter: adapter({
+      ipfsFixDisabled: false
+    })
+  }
 }
 
 export default config
